25ebdc8b53ed24fbf9220000ae84e0583ffb71d6
Logan Gundry <lgundry@nmu.edu>
2024-10-28 16:44:10 -0400

    Added a test for the Pillow plugin

---
+++ misc/pillow-test.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+jpg_filepath = "data/extracted_images/exists/exists_2225.jpg"
+
+jpg_plt_img = Image.open(jpg_filepath)
+
+jpg_as_np_array = np.asarray(jpg_plt_img)
+
+np.savetxt("outfile.txt", jpg_as_np_array)
