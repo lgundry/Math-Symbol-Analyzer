73156801df6759771b24742f5eb6903c114538b2
Logan Gundry <lgundry@nmu.edu>
2024-11-04 15:47:54 -0500

    modified:   misc/page-70-71.py 	new file:   misc/page-72.py

--- misc/page-70-71.py
+++ misc/page-70-71.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.datasets import make_classification
 
-# 1: create dummy dataset
+# 1: create dummy dataset and split it into class 0 and class 1 collections, stored in x0, y0 and x1, y1, respectively
 
 a,b = make_classification(n_samples=10000, weights=(0.9,0.1))
 idx = np.where(b == 0)[0]
@@ -11,12 +11,16 @@ idx = np.where(b == 1)[0]
 x1 = a[idx,:]
 y1 = b[idx]
 
+# 2: randomize the ordering
+
 idx = np.argsort(np.random.random(y0.shape))
 y0 = y0[idx]
 x0 = x0[idx]
 idx = np.argsort(np.random.random(y1.shape))
 y1 = y1[idx]
 x1 = x1[idx]
+
+# 3: extract the first 90% of the total in the dataset
 
 ntrn0 = int(0.9*x0.shape[0])
 ntrn1 = int(0.9*x1.shape[0])
@@ -27,6 +31,8 @@ xtrn[ntrn0:] = x1[:ntrn1]
 ytrn[:ntrn0] = y0[:ntrn0]
 ytrn[ntrn0:] = y1[:ntrn1]
 
+# 4: 5 extracat 5% for the validation set
+
 n0 = int(x0.shape[0]-ntrn0)
 n1 = int(x1.shape[0]-ntrn1)
 xval = np.zeros((int(n0/2+n1/2),20))
@@ -35,6 +41,8 @@ xval[:(n0//2)] = x0[ntrn0:(ntrn0+n0//2)]
 xval[(n0//2):] = x1[ntrn1:(ntrn1+n1//2)]
 yval[:(n0//2):] = y0[ntrn0:(ntrn0+n0//2)]
 yval[(n0//2):] = y1[ntrn1:(ntrn1+n1//2)]
+
+# 5: extract the final 5% for the test set
 
 xtst = np.concatenate((x0[(ntrn0+n0//2):],x1[(ntrn1+n1//2):]))
 ytst = np.concatenate((y0[(ntrn0+n0//2):],y1[(ntrn1+n1//2):]))
